28507d80e49962c3b522b623fdadbc4a01ea2bbd
Andy Crouse <acrouse77@gmail.com>
2017-08-23 15:08:01 -0500

    builds and bootstrap works

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -1,9 +1,19 @@
 import Vue from 'vue'
 import axios from 'axios'
-
 import App from './App'
+import BootstrapVue from 'bootstrap-vue'
 import router from './router'
 import store from './store'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'jquery/dist/jquery.min.js'
+import 'bootstrap/dist/js/bootstrap.min.js'
+// import 'font-awesome/4.3.0/css/font-awesome.min.css'
+// ADDED BY ANDY
+global.jQuery = require('jquery')
+window.jQuery = require('jquery')
+var $ = global.jQuery
+window.$ = $
+// require('bootstrap')
 
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.http = Vue.prototype.$http = axios
